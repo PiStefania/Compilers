7faf8993586847d5eb374c0e4885060b7bb2e7f2
Stefania <stefanoula-fani@hotmail.com>
2017-05-17 01:53:07 +0300

    edit done

--- skeleton.java/src/main/java/compiler/PrinterAST.java
+++ skeleton.java/src/main/java/compiler/PrinterAST.java
@@ -2,11 +2,31 @@ package compiler;
 
 import compiler.analysis.DepthFirstAdapter;
 import compiler.node.*;
+import compiler.analysis.*;
 import java.util.ArrayList;
 import java.util.List;
 
 
-public class PrinterAST extends DepthFirstAdapter{
+public class PrinterAST extends AnalysisAdapter{
+
+    private double value = 0;
+
+    @Override
+    public void caseStart(Start node) {
+        node.getPProgram().apply(this);
+    }
+
+    @Override
+    public void caseAAllFuncDef(AAllFuncDef node) {
+        node.getL().apply(this);
+        double lhs = value;
+
+        node.getR().apply(this);
+        double rhs = value;
+
+        value = lhs * rhs;
+        System.out.println(value);
+    }
 
 
 
